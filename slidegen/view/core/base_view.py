0fbe94822d9fa2da4d74461ac38208fb98f258ce
wjs <1550381401@qq.com>
2025-04-07 10:40:22 +0800

    🦄 refactor: refactor

--- slidegen/view/core/base_view.py
+++ slidegen/view/core/base_view.py
@@ -1,4 +1,3 @@
-from typing import Any, Union
 import inspect
 
 from fastapi import APIRouter, Depends
@@ -15,7 +14,7 @@ class BaseView:
     path: str = None
 
     def __init__(self, path: str, tags: list[str]):
-        self.path = path or getattr(self, "path")
+        self.path = path or self.path
         self.tags = tags or getattr(self, "tags", None)
         self.register_routes()
 
@@ -36,30 +35,27 @@ class BaseView:
         return g.user.role_ids
 
     @staticmethod
-    def response(code: int = 0, message: str = "请求成功", data: Union[dict, None, list, str] = None,
-                 pager: Pager = None):
+    def response(code: int = 0, message: str = "请求成功", data: dict | None | list | str = None, pager: Pager = None):
         if isinstance(data, list):
             if pager:
                 data = {"items": data or [], **pager.model_dump()}
             else:
                 data = {"items": data or []}
 
-        return {
-            "code": code,
-            "message": message,
-            "trace_id": g.trace_id,
-            "data": data or {}
-        }
+        return {"code": code, "message": message, "trace_id": g.trace_id, "data": data or {}}
 
     def get_dependencies(self, extra_params: dict, method):
-        custom_permission_classes = extra_params.pop('permission_classes', None)
-        custom_authentication_classes = extra_params.pop('authentication_classes', None)
-        authentication_classes = (custom_authentication_classes if custom_authentication_classes is not None
-                                  else self.authentication_classes)
-        permission_classes = (custom_permission_classes if custom_permission_classes is not None else
-                              self.permissions_classes)
-        dependencies = [Depends(_(name='')(method)) for _ in authentication_classes] + \
-                       [Depends(_(method)) for _ in permission_classes]
+        custom_permission_classes = extra_params.pop("permission_classes", None)
+        custom_authentication_classes = extra_params.pop("authentication_classes", None)
+        authentication_classes = (
+            custom_authentication_classes if custom_authentication_classes is not None else self.authentication_classes
+        )
+        permission_classes = (
+            custom_permission_classes if custom_permission_classes is not None else self.permissions_classes
+        )
+        dependencies = [Depends(_(name="")(method)) for _ in authentication_classes] + [
+            Depends(_(method)) for _ in permission_classes
+        ]
         if inspect.iscoroutinefunction(method):
             dependencies += [Depends(get_db)]
         else:
@@ -68,20 +64,36 @@ class BaseView:
 
     def register_routes(self):
         method_map = {
-            "get": {'path': self.path, 'methods': ['GET'], },
-            'post': {'path': self.path, 'methods': ['POST'], },
-            'put': {'path': self.path, 'methods': ['PUT'], },
-            'delete': {'path': self.path, 'methods': ['DELETE'], },
+            "get": {
+                "path": self.path,
+                "methods": ["GET"],
+            },
+            "post": {
+                "path": self.path,
+                "methods": ["POST"],
+            },
+            "put": {
+                "path": self.path,
+                "methods": ["PUT"],
+            },
+            "delete": {
+                "path": self.path,
+                "methods": ["DELETE"],
+            },
         }
         for method_name, router_info in method_map.items():
             method = getattr(self, method_name, None)
             if self.is_method_overridden(method_name):
-                extra_params = getattr(method, '_extra_params', {})
+                extra_params = getattr(method, "_extra_params", {})
                 dependencies = self.get_dependencies(extra_params, method)
-                base_router.add_api_route(router_info['path'], method,
-                                          methods=router_info['methods'],
-                                          dependencies=dependencies,
-                                          tags=self.tags, **extra_params)
+                base_router.add_api_route(
+                    router_info["path"],
+                    method,
+                    methods=router_info["methods"],
+                    dependencies=dependencies,
+                    tags=self.tags,
+                    **extra_params,
+                )
 
     def is_method_overridden(self, method_name: str) -> bool:
         subclass_method = getattr(self, method_name, None)
